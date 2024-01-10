758c8ba06462adb1441747e7a21a6fbb95938a78
PavanAnnaparthi21 <113789537+PavanAnnaparthi21@users.noreply.github.com>
2024-01-10 20:06:16 +0530

    prime prgm by m2

---
+++ prime1.js
@@ -0,0 +1,9 @@
+function isprime(number){
+    if(number <= 1)return false;
+    for(let i = 0; i < number; i++){
+        if(number % i === 0){
+            return false;
+        }
+    }
+    return true;
+}
