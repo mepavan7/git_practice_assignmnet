07343170d2be88c546844a6ae034205ae93d18c7
PavanAnnaparthi21 <113789537+PavanAnnaparthi21@users.noreply.github.com>
2024-01-10 22:49:17 +0530

    modefied code by m2

--- prime1.js
+++ prime1.js
@@ -7,3 +7,15 @@ function isprime(number){
     }
     return true;
 }
+function checkAndLogPrime(number){
+    if(isNaN(number)){
+        return "Please enter a valid numer."
+    }
+
+return isprime(number)
+?`${number} is a prime number.` 
+: `${number} is not a prime number`;
+}
+
+let num = 13;
+console.log(checkAndLogPrime(num));
